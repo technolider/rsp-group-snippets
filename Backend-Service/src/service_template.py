865317f6b91c01efeee3582c46b9ed4f83172f2f
Andrey <109785368+AndreyBry@users.noreply.github.com>
2022-07-24 12:41:11 +0500

    Add files via upload

---
+++ Backend-Service/src/service_template.py
@@ -0,0 +1,33 @@
+from interface import Interface
+
+
+class Service(Interface):
+    def __init__(self, url: str) -> None:
+        """Получение данных с SNS (data, last_target, pre_last_target) и функции send_update_status"""
+        super().__init__(url)
+
+        self.__update_status = {
+            'to': self.pre_last_target['service_name'],
+            'ray_id': int(self.data['ray_id']),
+            'status_update': 'OK',
+            'status_comment': 'something'
+        }
+
+    def is_callback_needed(self) -> bool:
+        """Нужны ли данные предыдущему сервису"""
+        return self.pre_last_target['is_callback_needed']
+
+    def give_data_last_service(self) -> None:
+        """Отправка данных о статусе"""
+        if self.is_callback_needed():
+            self.send_update_status(self.__update_status)
+
+    def set_status_update(self, status: str) -> None:
+        self.__update_status['status_update'] = status
+
+    def set_status_comment(self, comment: str) -> None:
+        self.__update_status['status_comment'] = comment
+
+
+serv = Service(url='http://192.168.141.61/protocol.json')
+serv.give_data_last_service()
