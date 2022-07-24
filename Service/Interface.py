4fdddaae7ed4838ccd8e0c7341f5af4a1ff80f6b
Dmitriy Gorbunov <dmitrgor1@yandex.ru>
2022-07-24 12:03:24 +0500

    определена структура сервиса

---
+++ Service/Interface.py
@@ -0,0 +1,18 @@
+import requests
+
+class Interface():
+    def __init__(self) -> None: # Инит функция в которой создаются 
+        pass
+    
+    def __connect_sns(self) -> None:
+        pass
+
+    def get_data(self) -> None:
+        pass
+
+    def package_data(self) -> None:
+        pass
+
+    def send_data(self) -> None:
+        pass
+    
