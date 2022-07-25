5f887b533aff308c35a0714c880048878ff5553e
Dmitriy Gorbunov <dmitrgor1@yandex.ru>
2022-07-25 12:42:08 +0500

    импортировал форму

---
+++ Frontend/rsp/src/pages/SignIn.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react'
+import logo from "../img/logo.png"
+import { Button, Checkbox, Form, Input } from 'antd';
+// import axios from 'axios';
+// import validator from 'validator';
+
+// const DOMEN_SERVER = '';
+// const DOMEN_SITE = '';
+
+export function SignIn() {
+  const onFinish = (values) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+
+  return (
+    <div className='items-center'>
+      <div className='items-center'>
+        <img src={logo} alt="" className='w-[20%] h-[20%]' />
+        <div className='items-center'>
+          <Form
+            name="basic"
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 16 }}
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+          >
+            <Form.Item
+              label="Username"
+              name="username"
+              rules={[{ required: true, message: 'Please input your username!' }]}
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[{ required: true, message: 'Please input your password!' }]}
+            >
+              <Input.Password />
+            </Form.Item>
+
+            <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+
+            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
+      </div>
+    </div>
+  )
+}
