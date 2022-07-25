7e2b9acbec335586522aca1200f6289cdef58680
Dmitriy Gorbunov <dmitrgor1@yandex.ru>
2022-07-25 17:21:10 +0500

    вход готов

--- Frontend/rsp/src/pages/SignIn.js
+++ Frontend/rsp/src/pages/SignIn.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import logo from "../img/logo.png"
+import { IdcardTwoTone, LockTwoTone } from '@ant-design/icons';
 import { Button, Checkbox, Form, Input } from 'antd';
 // import axios from 'axios';
 // import validator from 'validator';
@@ -9,55 +10,62 @@ import { Button, Checkbox, Form, Input } from 'antd';
 
 export function SignIn() {
   const onFinish = (values) => {
-    console.log('Success:', values);
+    console.log('Received values of form: ', values);
   };
-
-  const onFinishFailed = (errorInfo) => {
-    console.log('Failed:', errorInfo);
-  };
-
   return (
-    <div className='items-center'>
-      <div className='items-center'>
-        <img src={logo} alt="" className='w-[20%] h-[20%]' />
-        <div className='items-center'>
-          <Form
-            name="basic"
-            labelCol={{ span: 8 }}
-            wrapperCol={{ span: 16 }}
-            initialValues={{ remember: true }}
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-            autoComplete="off"
+      <div className='mt-[159px] text-center'>
+        <img src={logo} alt="" className='w-[20%] h-[20%] inline' />
+        <h1 className='font-medium text-[20px] mt-[29px] mb-[30px]'>Вход в кабинет</h1>
+        <Form
+          name="normal_login"
+          className="login-form items-centerx"
+          initialValues={{
+          emember: true,
+          }}
+          onFinish={onFinish}
+        >
+          <div className="ml-[539px]">
+          <Form.Item
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: 'Пожалуйста, введите свой логин!',
+              },
+            ]}
+            className='w-[40%]'
           >
-            <Form.Item
-              label="Username"
-              name="username"
-              rules={[{ required: true, message: 'Please input your username!' }]}
-            >
-              <Input />
+            <Input prefix={<IdcardTwoTone />} placeholder="Логин" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: 'Пожалуйста, введите свой пароль!',
+              },
+            ]}
+            className='w-[40%]'
+          >
+            <Input
+              prefix={<LockTwoTone />}
+              type="password"
+              placeholder="Пароль"
+            />
+          </Form.Item>
+          </div>
+          <Form.Item>
+            <Form.Item name="remember" valuePropName="checked" noStyle>
+              <Checkbox>Запомнить меня</Checkbox>
             </Form.Item>
+          </Form.Item>
 
-            <Form.Item
-              label="Password"
-              name="password"
-              rules={[{ required: true, message: 'Please input your password!' }]}
-            >
-              <Input.Password />
-            </Form.Item>
-
-            <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
-              <Checkbox>Remember me</Checkbox>
-            </Form.Item>
-
-            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-              <Button type="primary" htmlType="submit">
-                Submit
-              </Button>
-            </Form.Item>
-          </Form>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" className="login-form-button">
+              Войти
+            </Button>
+          </Form.Item>
+        </Form>
         </div>
-      </div>
-    </div>
   )
 }
